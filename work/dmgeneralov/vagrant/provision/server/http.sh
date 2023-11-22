e497af418b71b1c7f21940206884b35981ae2fc6
Danya Generalov <danya@danya02.ru>
2023-11-22 23:59:38 +0300

    Add lab 5.

--- work/dmgeneralov/vagrant/provision/server/http.sh
+++ work/dmgeneralov/vagrant/provision/server/http.sh
@@ -5,11 +5,20 @@ dnf -y groupinstall "Basic Web Server"
 echo "Copy configuration files"
 cp -R /vagrant/provision/server/http/etc/httpd/* /etc/httpd
 cp -R /vagrant/provision/server/http/var/www/* /var/www
-chown -R apache:apache /var/www
-restorecon -vR /etc
-restorecon -vR /var/www
+# ---
+echo "Copy certs"
+cp -R /vagrant/provision/server/http/etc/pki/* /etc/pki
+ln -s /etc/pki/tls/private /etc/ssl/private
+echo "Install PHP"
+dnf -y install php
 echo "Configure firewall"
 firewall-cmd --add-service=http
 firewall-cmd --add-service=http --permanent
+firewall-cmd --add-service=https
+firewall-cmd --add-service=https --permanent
+
+chown -R apache:apache /var/www
+restorecon -vR /etc
+restorecon -vR /var/www
 echo "Start http service"
 systemctl enable --now httpd
---
+++ work/dmgeneralov/vagrant/provision/server/http/var/www/html/www.dmgeneralov.net/index.php
@@ -0,0 +1,5 @@
+Hello from www.dmgeneralov.net!<br>
+Today we're running this version of PHP:<br>
+<?php
+phpinfo();
+?>
